6c144899b2c7fed1793ef259d958ce1f7409aeb2
LorAutumn <lorenz.herbst@gmx.net>
2023-11-13 21:45:18 +0100

    add readme and comments

--- 00 Prompting/temperature.py
+++ 00 Prompting/temperature.py
@@ -10,6 +10,8 @@ env_vars = dotenv_values(".env")
 
 apikey = env_vars["OPENAI_API_KEY"]
 
+# set temperature between 0 and 2 to see effects in outputet creativity
+
 llm = OpenAI(openai_api_key=apikey, max_tokens=1000, temperature=0.7, streaming=True,
              callbacks=[StreamingStdOutCallbackHandler()])
 
---
+++ README.md
@@ -0,0 +1,11 @@
+### Installation
+
+Install requirements.txt
+
+```bash
+pip install -r requirements.txt
+```
+
+Copy .env.dev to .env and insert OpenAI API key
+
+Run each .py individually
